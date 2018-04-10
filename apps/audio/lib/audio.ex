659be1dc43fbea88ffc6afc08637db413895c1dc
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2018-04-10 18:25:23 +0300

    Added mplayer draft

---
+++ apps/audio/lib/audio.ex
@@ -0,0 +1,18 @@
+defmodule Audio do
+  @moduledoc """
+  Documentation for Audio.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Audio.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
